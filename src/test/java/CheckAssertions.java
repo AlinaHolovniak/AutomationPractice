dab7be1e3e894d0a98e7bf4ab882b51fc83a8cbc
Alina Holovniak <golovniakalina@gmail.com>
2020-02-24 04:49:33 +0200

    Class CheckAssertions has been added

---
+++ src/test/java/CheckAssertions.java
@@ -0,0 +1,42 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class CheckAssertions {
+    String str1 = "aaa";
+    String str2 = "aaa";
+    String str3 = "bbb";
+
+
+    @Test
+    public void test1() {
+        Assert.assertEquals(str1, str2);
+    }
+
+    @Test
+    public void test2() {
+        Assert.assertEquals(str1, str3);
+    }
+
+    @Test
+    public void test3() {
+        Assert.assertTrue(1 == 1);
+    }
+
+    @Test
+    public void test4() {
+        Assert.assertTrue(1 == 2);
+    }
+
+    @Test
+    public void test5() {
+        Assert.assertFalse(1 == 2);
+    }
+
+    @Test
+    public void test6() {
+        Assert.assertFalse(1 == 1);
+    }
+}
+
+
+
